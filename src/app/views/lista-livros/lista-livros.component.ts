0ac148a2fe6dab5e89cc0fc26f1df1a4c6440a4c
Gean Filho <98719228+GeanFilho@users.noreply.github.com>
2024-05-11 19:52:09 -0300

    projeto biblioteca angular

---
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { catchError, debounceTime, filter, map, of, switchMap, tap, throwError } from 'rxjs';
+import { Item, LivrosResultado } from 'src/app/models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
+import { LivroService } from 'src/app/service/livro.service';
+
+const PAUSA = 300;
+@Component({
+  selector: 'app-lista-livros',
+  templateUrl: './lista-livros.component.html',
+  styleUrls: ['./lista-livros.component.css']
+})
+export class ListaLivrosComponent {
+
+  campoBusca = new FormControl();
+  mensagemErro = '';
+  livrosResultado: LivrosResultado;
+
+  constructor( private service: LivroService) { }
+
+  totalDeLivros$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    tap(() => console.log('Fluxo inicial')),
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    map(resultado => this.livrosResultado = resultado),
+    catchError(erro => {
+      console.log(erro)
+      return of()
+    })
+  )
+
+  livroEncontrados$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    tap(() => console.log('Fluxo inicial')),
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    tap((retornoAPI) => console.log(retornoAPI)),
+    map(resultado => resultado.items ?? []),
+    map(items =>
+       this.livrosResultadoParaLivros(items)),
+    catchError(erro => {
+      console.log(erro)
+      return throwError(() => new Error(this.mensagemErro ='Erro, recarregue a página!'))
+    })
+  )
+
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map(item => {
+      return new LivroVolumeInfo(item)
+    })
+  }
+
+}
+
+
+
